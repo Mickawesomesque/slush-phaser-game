0760040aa9592a143030526049343bebfcfa482b
Mickaël Mendel <mickael.mendel@gmail.com>
2014-07-17 20:31:32 +0200

    The template is now a little demo with a few different states

--- templates/app/src/scripts/states/boot.js
+++ templates/app/src/scripts/states/boot.js
@@ -8,13 +8,13 @@ Boot.prototype.constructor = Boot;
 
 Boot.prototype.preload = function () {
   this.load.baseURL = './assets/';
-  this.load.image('progressBar', 'progressBar.png');
+  // this.load.image('progressBar', 'progressBar.png');
 };
 
 Boot.prototype.create = function () {
   this.scale.pageAlignHorizontally = true;
   this.scale.pageAlignVertically = true;
-  this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+  // this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
   this.scale.setScreenSize();
 
   this.game.state.start('preloader');
